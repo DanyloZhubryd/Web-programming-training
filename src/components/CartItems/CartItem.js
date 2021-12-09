5d37a06348443af905b4ca7c723e4cacefe4d036
Zhubryd Danylo <zubdan21@gmail.com>
2021-12-09 10:05:20 +0200

    Fix Cart view

--- src/components/CartItems/CartItem.js
+++ src/components/CartItems/CartItem.js
@@ -3,9 +3,22 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { increaseAmount, decreaseAmount, removeFromCart } from "redux/actions/cartActions";
 import { Buttons, CardWrapper } from "./CartItem.styled";
-
+import BirdCard from "components/CardItem/BirdCard";
+import cockatiel from "Icons/Cockatiel.jpg";
+import parakeet from "Icons/Large-parakeet.jpg";
+import eclectus from "Icons/Eclectus.jpg";
 
 export const CartItem = (props) => {
+  let image;
+    if (props.item.image.toString().toLocaleLowerCase().search("cockatiel") !== -1){
+        image = cockatiel;
+    } 
+    else if (props.item.image.toString().toLocaleLowerCase().search("parakeet") !== -1){
+        image = parakeet;
+    }
+    else {
+        image = eclectus;
+    }
     const cartItems = useSelector((state) => state.cartItems.cartItems);
     let dispatch = useDispatch();
   
@@ -22,12 +35,14 @@ export const CartItem = (props) => {
       }
   }
     return (
-      <CardWrapper>
-        <Image src={props.item.image}/>
+      <BirdCard title={props.item.name}
+      image={image}
+      price={props.item.price}
+      id={props.item.id}
+      additional={
         <Buttons>
           <Button onClick={handleAmountIncrease}>+</Button>
           <Button onClick={handleAmountDecrease}>-</Button>
-        </Buttons>
-      </CardWrapper>
+        </Buttons>}/>
     );
     }
